23a32a2056bda8ad5b3c51edc7b3b0826c06554f
angel-kanade <iskrebs@outlook.com>
2025-12-05 15:49:20 +0800

    Basic Server Function Commited

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "Go_Zinx/znet"
+
+func main() {
+	// 创建一个服务器
+	s := znet.NewServer("[kanade's server]")
+
+	// 启动服务器
+	s.Serve()
+}
--- znet/server.go
+++ znet/server.go
@@ -1,5 +1,11 @@
 package znet
 
+import (
+	"Go_Zinx/zinterface"
+	"fmt"
+	"net"
+)
+
 // IServer的接口实现，定义一个Server的服务器模块
 type Server struct {
 	Name      string
@@ -9,13 +15,60 @@ type Server struct {
 }
 
 func (s *Server) Start() {
+	fmt.Printf("[Start] Server Listener at Address: %s:%d\n", s.IP, s.Port)
+
+	// 开辟一个 go 协程处理服务器启动，防止阻塞
+	go func() {
+		// 1. 获取一个TCP的Addr:Port
+		addr, err := net.ResolveTCPAddr(s.IPVersion, fmt.Sprintf("%s:%d", s.IP, s.Port))
+		if err != nil {
+			fmt.Println("Resolve TCP Address error", err)
+		}
+		// 2. 监听
+		listener, err := net.ListenTCP(s.IPVersion, addr)
+		if err != nil {
+			fmt.Println("Start Server Listener failed", err)
+		}
+
+		fmt.Println("start Zinx Server success", s.Name, "Listening")
+
+		// 3. 阻塞等待客户端连接，处理业务
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Accept Error", err)
+				continue
+			}
+
+			// 已经建立连接，开辟一个go协程，处理业务
+			go func() {
+
+			}()
+		}
+	}()
 
 }
 
 func (s *Server) Serve() {
+	s.Start()
 
+	// TODO 做一些启动服务器之外的额外业务
+
+	// 保持阻塞状态
+	select {}
 }
 
 func (s *Server) Stop() {
 
+}
+
+func NewServer(name string) zinterface.IServer {
+	s := &Server{
+		Name:      name,
+		IPVersion: "tcp4",
+		IP:        "127.0.0.1",
+		Port:      8888,
+	}
+
+	return s
 }
